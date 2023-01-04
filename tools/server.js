9e7b88f2fd0dcc99e5d0e3e28e2955b991dd1574
saramangelo <saramangelo@gmail.com>
2023-01-04 14:35:49 -0800

    updated server.js to include libraries, initialize port, app, middleware

---
+++ tools/server.js
@@ -0,0 +1,18 @@
+// require libraries
+const express = require('express');
+const path = require('path');
+const fs = require('fs');
+
+// initialize port
+const PORT = 3001;
+
+// initialize app to run express()
+const app = express();
+
+// middleware
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+// essentially telling express that anything static comes from the public folder
+app.use(express.static('public'));
+
