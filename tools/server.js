5c229b07dd87c2d0fe4c9123c8857347278ee2a7
saramangelo <saramangelo@gmail.com>
2023-01-04 14:52:10 -0800

    added notes

--- tools/server.js
+++ tools/server.js
@@ -16,3 +16,8 @@ app.use(express.urlencoded({ extended: true }));
 // essentially telling express that anything static comes from the public folder
 app.use(express.static('public'));
 
+// GET request
+
+// POST request
+
+// at the bottom - listener ~ app.listen(PORT, () =>)
