1867f4e80029792a4ea6b8a21c4db6ffabaed131
saramangelo <saramangelo@gmail.com>
2023-01-05 11:06:33 -0800

    added notes from class example

--- server.js
+++ server.js
@@ -92,3 +92,32 @@ res.send('DELETE request called');
 app.listen(PORT, () =>
 console.log(`App is listening on port ${PORT}!`)
 );
+
+
+// Try this? From Day 25, 22-Stu
+// // GET Route for retrieving all the tips
+// app.get('/api/tips', (req, res) => {
+//   console.info(`${req.method} request received for tips`);
+//   readFromFile('./db/tips.json').then((data) => res.json(JSON.parse(data)));
+// });
+
+// // POST Route for a new UX/UI tip
+// app.post('/api/tips', (req, res) => {
+//   console.info(`${req.method} request received to add a tip`);
+
+//   const { username, topic, tip } = req.body;
+
+//   if (req.body) {
+//     const newTip = {
+//       username,
+//       tip,
+//       topic,
+//       tip_id: uuid(),
+//     };
+
+//     readAndAppend(newTip, './db/tips.json');
+//     res.json(`Tip added successfully 🚀`);
+//   } else {
+//     res.error('Error in adding tip');
+//   }
+// });
