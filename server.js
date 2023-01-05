bc12a837a67f205707c2f03574d6f6fddb289af8
saramangelo <saramangelo@gmail.com>
2023-01-04 22:53:02 -0800

    added logic to read file, write file

--- server.js
+++ server.js
@@ -30,9 +30,51 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     // log that POST request was received
 console.info(`${req.method} request received to add notes`);
+
+// destructure assignment for items in req.body
+const { noteTitle, noteText } = req.body;
+
+// if both properties are present
+if (noteTitle && noteText) {
+    // variable for object we want to save
+    const newNote = {
+        noteTitle,
+        noteText,
+    };
+
+// convert data to string so we can save it
+const noteString = JSON.stringify(newNote);
+
+fs.readFile(`./db/db.json`, "utf-8", (err,data) => {
+
+    // convert string to JSON object
+    let noteArr = JSON.parse(noteString);
+    // add new note to array
+    noteArr.push(newNote);
+
+    // (stringify array) write string to a file 
+    fs.writeFile(`./db/db.json`, JSON.stringify(noteArr, null, "\t"), (err) =>
+    err
+    ? console.error(err)
+    : console.log(
+        `Note for ${newNote.noteTitle} has been written to JSON file`
+    )
+    );
 })
 
-// DELETE request? (bonus)
+const response = {
+    status: 'success',
+    body: newNote,
+};
+
+console.log(response);
+res.status(201).json(response);
+} else {
+    res.status(500).json('Error in posting note');
+}
+});
+
+// DELETE request? (bonus) ~ unsure about path..should it be just '/' instead?
 app.delete(`/api/notes${id}`, (req, res) => {
 res.send('DELETE request called');
 })
