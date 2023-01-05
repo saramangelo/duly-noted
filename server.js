2c38be59035cd14373cfe17395dba9dc8e8014f8
saramangelo <saramangelo@gmail.com>
2023-01-04 22:36:47 -0800

    added get, post, delete request and listener

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require('express');
 const path = require('path');
 const fs = require('fs');
+const { addAbortSignal } = require('stream');
 
 // initialize port
 const PORT = 3001;
@@ -17,7 +18,26 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
 // GET request
+app.get('/api/notes', (req, res) => {
+    // send message to client
+    res.json(`${req.method} request received to get notes`);
+
+    // log our request to terminal
+    console.info(`${req.method} request received to get notes`);
+});
 
 // POST request
+app.post('/api/notes', (req, res) => {
+    // log that POST request was received
+console.info(`${req.method} request received to add notes`);
+})
+
+// DELETE request? (bonus)
+app.delete(`/api/notes${id}`, (req, res) => {
+res.send('DELETE request called');
+})
 
 // at the bottom - listener ~ app.listen(PORT, () =>)
+app.listen(PORT, () =>
+console.log(`App is listening on port ${PORT}!`)
+);
